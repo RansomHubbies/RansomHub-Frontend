d5cdca86d8fbc7e0e85728a194d16384154f0e2d
CKBanka <chirag21142@iiitd.ac.in>
2025-03-07 19:09:20 +0530

    some changes to eslint

--- ransomhub/eslint.config.mjs
+++ ransomhub/eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off", // Allow "any" type
+      "react/no-unescaped-entities": "off", // Disable apostrophe escaping warning
+    },
+  },
 ];
 
 export default eslintConfig;
